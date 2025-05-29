879f0e049abb661e4e34e16b088ce9c159082152
sorocode <tudgus010707@naver.com>
2025-05-29 22:17:25 +0900

    :art: 이미지파일 반영을 위한 메소드 추가

--- src/main/java/model/MenuItem.java
+++ src/main/java/model/MenuItem.java
@@ -3,10 +3,12 @@ package model;
 public abstract class MenuItem {
     protected String name;
     protected int price;
+    protected String imagePath; // 이미지 추가 위한 변수
 
-    public MenuItem(String name, int price) {
+    public MenuItem(String name, int price, String imagePath) {
         this.name = name;
         this.price = price;
+        this.imagePath = imagePath;
     }
 
     public String getName() {
@@ -16,6 +18,10 @@ public abstract class MenuItem {
     public int getPrice() {
         return price;
     }
+
+    public String getImagePath() {
+        return imagePath;
+    } // 이미지 경로 getter 함수, MenuRepository에서 menuList.add(new Coffee("아메리카노", 3000, "images/americano.png")); 와 같이 생성 가능
 
     // 공통적으로 사용할 메소드(카테고리를 반환)
     public abstract String getCategory();
