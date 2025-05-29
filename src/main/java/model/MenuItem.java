7cf1b57a3ba868ca155f9ece15d5cf432c2f65f0
sorocode <tudgus010707@naver.com>
2025-05-29 22:22:45 +0900

    :art: 이미지 추가를 위한 메서드 추가

--- src/main/java/model/MenuItem.java
+++ src/main/java/model/MenuItem.java
@@ -21,7 +21,7 @@ public abstract class MenuItem {
 
     public String getImagePath() {
         return imagePath;
-    } // 이미지 경로 getter 함수, MenuRepository에서 menuList.add(new Coffee("아메리카노", 3000, "images/americano.png")); 와 같이 생성 가능
+    }
 
     // 공통적으로 사용할 메소드(카테고리를 반환)
     public abstract String getCategory();
