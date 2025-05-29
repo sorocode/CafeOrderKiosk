234563751356387d20d1d06ba6e329fa4c4032b1
sorocode <tudgus010707@naver.com>
2025-05-29 22:25:28 +0900

    :sparkles: OrderController 작성

--- src/main/java/controller/OrderController.java
+++ src/main/java/controller/OrderController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import model.MenuItem;
 import model.MenuRepository;
 import model.Order;
 import view.KioskMainView;
@@ -19,15 +20,49 @@ public class OrderController {
         this.view = view;
     }
 
-    // TODO: 메뉴 버튼 클릭 시 호출되는 메서드
+    /**
+     * 메뉴 버튼 클릭 시 호출(옵션 없을 경우)
+     */
     public void handleMenuSelection(String menuName) {
+        MenuItem item = menuRepository.findByName(menuName);
+        if (item != null) {
+            order.addItem(item);
+            view.updateOrderSummary(order.getOrderSummary());
+        } else {
+            view.showError("해당 메뉴를 찾을 수 없습니다: " + menuName);
+        }
     }
 
-    // TODO: 초기화 버튼 클릭 시 호출되는 메서드
+    /**
+     * 메뉴 선택(옵션 반영을 위한 메소드 오버로딩)
+     */
+    public void handleMenuSelection(MenuItem item) {
+        order.addItem(item);
+        view.updateOrderSummary(order.getOrderSummary());
+    }
+
+    /**
+     * '초기화' 버튼 클릭 시 호출
+     */
     public void handleClearOrder() {
+        order.clearItems();
+        view.updateOrderSummary("주문이 초기화되었습니다.");
     }
 
-    // TODO: 결제 버튼 클릭 시 호출되는 메서드
+    /**
+     * '결제' 버튼 클릭 시 호출
+     */
     public void handlePayment() {
+        if (order.getItems().isEmpty()) {
+            view.showError("주문 내역이 없습니다.");
+            return;
+        }
+
+        int total = order.getTotalPrice();
+        view.showMessage("결제 완료! 총액: " + total + "원");
+
+        order.clearItems();
+        view.updateOrderSummary(""); // 주문창 비우기
     }
+
 }
